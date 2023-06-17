630dff67d9817c6251638dab4fe605a1bff0c75e
Tina Shi <init.defaultBranch>
2023-06-17 22:52:55 +1000

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+
+    # Custom Validation
+
+    def title_is_clickbait
+        if title == 'True Facts'
+            errors.add(:title, "Sorry not allowed")
+        end
+    end
 end
